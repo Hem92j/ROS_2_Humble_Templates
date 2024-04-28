4f0c86bbb1b0b01273830cf8783aa19f9f58ff40
Hem92j <rauljihemraj@gmail.com>
2024-04-28 14:52:04 +0200

    updated publisher package

---
+++ src/simple_publisher_template/include/simple_publisher_template/simple_publisher.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+class SimplePublisher : public rclcpp::Node {
+
+public:
+    SimplePublisher();
+
+private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    size_t count;
+    void timer_callback();
+
+};
---
+++ src/simple_publisher_template/src/simple_publisher.cpp
@@ -0,0 +1,36 @@
+#include "simple_publisher_template/simple_publisher.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include <chrono>
+#include <memory>
+#include <functional>
+
+SimplePublisher::SimplePublisher() : Node("simple_publisher"), count(0) 
+{
+
+    publisher_ = this-> create_publisher<std_msgs::msg::String>("my_publisher", 10);
+
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(200), 
+                                     std::bind(&SimplePublisher::timer_callback, this));
+
+    RCLCPP_INFO(this->get_logger(), "Publisher initialized...");
+} 
+
+void SimplePublisher::timer_callback() {
+
+    auto msg = std_msgs::msg::String();
+    msg.data = "I am publishing data. count:  " + std::to_string(count++);    
+    publisher_->publish(msg);
+
+}
+
+
+int main(int argc, char** argv) {
+
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SimplePublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+
+    return 0;
+}
